5873f5aaaf66cd0f0e76ca5122ba783d4bd36fd0
seon0313 <seon06.dev@gmail.com>
2025-05-21 18:19:43 +0900

    fixed variable name

--- server.py
+++ server.py
@@ -9,30 +9,30 @@ from log import LogType
 
 class Server:
     def __init__(self, system: RobotSystem, host='0.0.0.0', port=8765):
-        self.__host__ = host
-        self.__port__ = port
-        self.__server__: websockets.serve = None
+        self.__host = host
+        self.__port = port
+        self.__server: websockets.serve = None
 
-        self.__system__ = system
-        self.el = EventListener(self.__listener__)
-        self.__system__.event.addListener('server', self.el)
-        self.__system__.log.i(LogType.SERVER, "Server Load END")
+        self.__system = system
+        self.el = EventListener(self.__listener)
+        self.__system.event.addListener('server', self.el)
+        self.__system.log.i(LogType.SERVER, "Server Load END")
 
-    def __listener__(self, *arg):
-        self.__system__.log.i(LogType.SERVER, f"change value: {self.__system__.values}")
+    def __listener(self, *arg):
+        self.__system.log.i(LogType.SERVER, f"change value: {self.__system.values}")
         #print('server Listener - port: ', self.__port__, 'args: ', arg)
 
-    def __base64_to_image__(self, base64_string):
+    def __base64_to_image(self, base64_string):
         img_data = base64.b64decode(base64_string)
         np_arr = np.frombuffer(img_data, np.uint8)
         img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         return img
     
-    async def __client__(self, websocket):
+    async def __client(self, websocket):
         #try:
         client_id = id(websocket)
         _ip, _port = websocket.remote_address
-        self.__system__.log.i(LogType.SERVER,
+        self.__system.log.i(LogType.SERVER,
                                 f"Connected New Client id: {client_id} | {_ip}:{_port}")
         #try:
         while True:
@@ -40,26 +40,26 @@ class Server:
             msg = json.loads(msg)
 
             if msg['type'] == 'get':
-                self.__system__.log.w(LogType.SERVER, f"{self.__system__.values}")
-                await websocket.send(json.dumps(self.__system__.values))
+                self.__system.log.w(LogType.SERVER, f"{self.__system.values}")
+                await websocket.send(json.dumps(self.__system.values))
         #except Exception as e:
         #    self.__system__.log.e(LogType.SERVER, f'Error CLient id: {client_id} | {e}')
-        self.__system__.log.i(LogType.SERVER,
+        self.__system.log.i(LogType.SERVER,
                                 f"Disconnected Client id: {client_id} | {_ip}:{_port}")
         #except Exception as e:
         #    self.__system__.log.e(LogType.SERVER,f"Client Error {e}")
-    async def __start_server__(self):
+    async def __start_server(self):
         try:
-            self.__system__.log.i(LogType.SERVER, "Opening Server...")
-            self.__server__ = await websockets.serve(self.__client__, self.__host__, self.__port__)
-            self.__system__.log.i(LogType.SERVER, "Server Opened")
-            await self.__server__.wait_closed()
+            self.__system.log.i(LogType.SERVER, "Opening Server...")
+            self.__server = await websockets.serve(self.__client, self.__host, self.__port)
+            self.__system.log.i(LogType.SERVER, "Server Opened")
+            await self.__server.wait_closed()
         except Exception as e:
-            self.__system__.log.e(LogType.SERVER,
+            self.__system.log.e(LogType.SERVER,
                                   f"Start Error - {e}")
     def close(self):
-        if self.__server__:
-            self.__server__.close()
-        self.__system__.log.i(LogType.SERVER, "Server Close")
+        if self.__server:
+            self.__server.close()
+        self.__system.log.i(LogType.SERVER, "Server Close")
     def run(self):
-        asyncio.run(self.__start_server__())
+        asyncio.run(self.__start_server())
--- system.py
+++ system.py
@@ -9,16 +9,16 @@ class EventListener:
 
 class Event:
     def __init__(self):
-        self.__l__: dict[EventListener] = {}
+        self.__l: dict[EventListener] = {}
     
     def sendEvent(self,name: str, value: any) -> bool:
-        eListner: EventListener = self.__l__.get(name)
+        eListner: EventListener = self.__l.get(name)
         if eListner:
             eListner.getEvent(value)
             return True
         return False
     def addListener(self,name:str, listener: EventListener):
-        self.__l__[name] = listener
+        self.__l[name] = listener
 
 class RobotSystem:
     def __init__(self, event: Event, log: Log):
