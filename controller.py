6a616804d0493b2987386ad469ea7ba762aa0178
seon0313 <seon06.dev@gmail.com>
2025-05-22 22:07:38 +0900

    add move system

--- controller.py
+++ controller.py
@@ -2,6 +2,17 @@ from system import RobotSystem, Event, EventListener
 from log import Log, LogType
 import threading
 import time
+from enum import Enum
+
+class ValueType(Enum):
+    RANGE = 0
+    VALUE = 1
+
+
+class Value:
+    def __init__(self, valueType: ValueType, value: any):
+        self.valueType = valueType
+        self.value = value
 
 class Controller:
     def __init__(self, system: RobotSystem):
@@ -9,13 +20,19 @@ class Controller:
         self.__system.log.i(LogType.CONTROLLER, "Controller Load end")
         self.__handle: dict = None
         self.__system.event.addListener('controller', EventListener(self.__listener))
-        self.__moveAngles: dict = {'front': 0, 'back': 0, 'left': 0, 'right': 0}
+        self.__moveAngles: dict = {
+            'front': Value(ValueType.RANGE, 0),
+            'back': Value(ValueType.RANGE, 0),
+            'left': Value(ValueType.RANGE, 0),
+            'right': Value(ValueType.RANGE, 0),
+            'rocate': Value(ValueType.VALUE, 0),
+            }
     
     def __listener(self, *arg):
         self.__handle = arg[0]
         if self.__handle.get('direction') in self.__moveAngles.keys():
-            self.__moveAngles[self.__handle.get('direction')] = self.__handle.get('value', False)
-            self.__system.log.i(LogType.CONTROLLER, f"Move: {self.__handle.get('direction')} value: {self.__moveAngles[self.__handle.get('direction')]}")
+            self.__moveAngles[self.__handle.get('direction')].value = self.__handle.get('value', 0)
+            self.__system.log.i(LogType.CONTROLLER, f"Move: {self.__handle.get('direction')} value: {self.__moveAngles[self.__handle.get('direction')].value}")
     
     def run(self, event: threading.Event):
         while not event.is_set():
