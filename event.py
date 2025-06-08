ef368db305fa6d791cd90c88f4567ad3e86b9731
seon0313 <seon06.dev@gmail.com>
2025-06-08 23:45:59 +0900

    disable all functions and event end

--- event.py
+++ event.py
@@ -14,48 +14,52 @@ class Event:
         self.__system = system
         self.__log = log
     
-    def __client(self, client: socket.socket, addr,id:str):
+    def __client(self, client: socket.socket, addr, id:str):
+        self.__log.i(LogType.EVENT, f'connected id:{id} | {addr}')
         if not self.__clients.get(id, None): self.__clients[id] = [client]
         else: self.__clients[id].append(client)
+
+        overflow: str = ''
+
         while not self.__threadEvent.is_set():
             try:
-                data = bytearray()
-                while True:
-                    packet = client.recv(1024)
-                    if not packet: break
-                    data.extend(packet)
-                data: dict = json.loads(data.decode('utf8'))
+                data = str(overflow+(client.recv(4096).decode('utf8'))).split('\0')
+
+                if len(data)>1:
+                    overflow = data[1]
+                    if len(data) >= 3: overflow += '\0'
+                else: overflow = ''
+
+                data: dict = json.loads(data[0])
 
                 type_ = data.get('type', '')
 
                 if type_ == 'send':
                     id_ = data.get('target', None)
                     if id_:
-                        msg = json.dumps(data).encode('utf8')
+                        msg = json.dumps(data.get('value', '')).encode('utf8')
                         for i in self.__clients.get(id_, []):
                             i.sendall(msg)
-                elif type_ == 'log':
+                if type_ == 'log':
                     logType = LogType.getLogType(data.get('logType'))
                     if logType:
-                        messageType = LogMessageType.getLogMessageType(data.get('message'))
+                        messageType = LogMessageType.getLogMessageType(data.get('messageType'))
                         if messageType:
                             message = data.get('message', '')
                             self.__log.addLog(logType, messageType, message)
 
             except Exception as e:
-                self.__system.event.e(LogType.EVENT, f"Client Error {addr}:\t{e}")
-            self.__clients[id].remove(client)
-            self.__system.event.i(LogType.EVENT, f'Client Close {addr}')
+                self.__log.e(LogType.EVENT, f"Client Error {addr}:\t{e}")
+        self.__clients[id].remove(client)
+        self.__log.i(LogType.EVENT, f'Client Close {addr}')
 
     def __run(self):
         self.__server.bind((self.__ip, self.__port))
         self.__server.listen()
         while not self.__threadEvent.is_set():
             client, addr = self.__server.accept()
-            id = (client.recv(1024))
-            threading.Thread(target=self.__client, args=(client, addr,id,))
-
-        
+            id = client.recv(1024).decode('utf8')
+            threading.Thread(target=self.__client, args=(client, addr,id,)).start()
 
     def run(self):
         self.__thread.start()
@@ -65,14 +69,23 @@ class Event:
         self.__thread.join()
 
 class EventListener:
-    def __init__(self, func: any, ip='0.0.0.0', port=7564):
+    def __init__(self, name: str, func: any, ip='0.0.0.0', port=7564):
         self.__ip = ip
+        self.name = name
         self.__port = port
         self.__client: socket.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.__thread: threading.Thread = threading.Thread(target=self.__run, daemon=True)
+        self.__thread: threading.Thread = None
         self.__threadEvent: threading.Event = threading.Event()
         self.__has_connect: bool = False
         self.func = func
+
+    def sendEvent(self, name: str, value: any):
+        msg = {
+            'type': 'send',
+            'target': name,
+            'value': value
+        }
+        self.__client.sendall(str(json.dumps(msg)+'\0').encode('utf8'))
 
     def sendLog(self, logType: LogType, messageType: LogMessageType, message: str):
         msg = {
@@ -81,7 +94,7 @@ class EventListener:
             'messageType': messageType.value,
             'message': message
         }
-        self.__client.send(json.dumps(msg).encode('utf8'))
+        self.__client.send(str(json.dumps(msg)+'\0').encode('utf8'))
 
     def i(self, logType: LogType, message: str):
         self.sendLog(logType, LogMessageType.NORMAL, message)
@@ -94,16 +107,15 @@ class EventListener:
         while not self.__has_connect and not self.__threadEvent.is_set(): pass
 
         while not self.__threadEvent.is_set():
-            data = bytearray()
-            while True:
-                packet = self.__client.recv(1024)
-                if not packet: break
-                data.extend(packet)
-            data: dict = json.dumps(data.decode('utf8'))
-            self.func(data)
+            try:
+                data = self.__client.recv(4096)
+                data: dict = json.loads(data.decode('utf8'))
+                self.func(data)
+            except Exception as e: self.sendLog(LogType.EVENT, LogMessageType.ERROR, f'L:\t{e}')
 
     def run(self):
-        self.__client.connect((self.ip, self.__port))
+        self.__client.connect((self.__ip, self.__port))
+        self.__client.sendall(self.name.encode('utf8'))
         self.__has_connect = True
-        self.__thread = threading.Thread(self.__listener, daemon=True)
+        self.__thread = threading.Thread(target=self.__listener, daemon=True)
         self.__thread.start()
--- robot.py
+++ robot.py
@@ -29,18 +29,15 @@ class Robot:
             version=self.robot_version
         )
 
-        self.__event: Event = Event(self.system)
+        self.__event: Event = Event(self.system, self.__log)
 
-        self.__server: Server = Server(self.system)
+        #self.__server: Server = Server(self.system)
         with open('./key.txt', 'r') as f:
             OPENAI_API_KEY = f.readline()
             f.close()
         self.__realTime: RealTime = RealTime(OPENAI_API_KEY)
 
-        self.__controller: Controller = ABBController(self.system)
-
-        self.system.event.sendEvent('server', 'Hello!')
-        self.system.event.addListener('log', EventListener(self.__logListener))
+        #self.__controller: Controller = ABBController(self.system)
 
         self.__aiThread: mp.Process = None
         self.__aiThreadEvent = mp.Event()
@@ -53,51 +50,43 @@ class Robot:
         self.__broadcastServerThread: threading.Thread = None
         self.__broadcastServerThreadEvent: threading.Event = threading.Event()
 
-        self.system.event.i(LogType.ROBOT, "Load END")
-    
-    def __logListener(self, data: dict):
-        mt = data.get('logMessageType', None)
-        lt = data.get('logType')
-        me = data.get('message')
-        ty = data.get('type')
-        if ty == 'close':
-            self.__log.close()
-        if mt == LogMessageType.NORMAL:
-            self.__log.i(lt, me)
-        elif mt == LogMessageType.ERROR:
-            self.__log.e(lt, me)
-        elif mt == LogMessageType.WARNING:
-            self.__log.w(lt, me)
+        self.__Listener = EventListener('test',lambda x: self.__log.i(LogType.EVENT, f'I get Event {x}'))
+
+        self.__log.i(LogType.ROBOT, "Load END")
 
     def runServer(self):
         self.__server.run(self.__serverThreadEvent)
         self.__server.close()
-        self.system.event.i(LogType.SERVER, "Server Thread Close")
+        self.__log.i(LogType.SERVER, "Server Thread Close")
 
     def runAI(self):
-        self.system.event.i(LogType.AI, "AI Thread Close")
+        self.__log.event.i(LogType.AI, "AI Thread Close")
 
     def controller(self):
         self.__controller.run(self.__controllerThreadEvent)
-        self.system.event.i(LogType.CONTROLLER, "Controller Thread Close")
+        self.__log.event.i(LogType.CONTROLLER, "Controller Thread Close")
 
     def background(self):
-        def view(c):
-            print('!', flush=True)
-            cv2.imshow('get image', c[0])
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-        self.system.event.addListener('camera',
-                                      EventListener(view))
+        #def view(c):
+        #    print('!', flush=True)
+        #    cv2.imshow('get image', c[0])
+        #    if cv2.waitKey(1) & 0xFF == ord('q'):
+        #            cv2.destroyAllWindows()
+        #self.system.event.addListener('camera',
+        #                              EventListener(view))
+        self.__Listener.run()
+        count = 0
         while not self.__backgroundThreadEvent.is_set():
-            #self.system.event.sendEvent('server', '!')
+            self.__Listener.sendEvent('test', f'{count}')
+            self.__Listener.i(LogType.BACKGROUND, f'send {count}')
+            count+=1
             time.sleep(1)
-        self.system.event.i(LogType.BACKGROUND, "Background Thread Close")
+        self.__log.i(LogType.BACKGROUND, "Background Thread Close")
     
     def broadcast(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.system.event.i(LogType.BROADCAST, "BroadCast On")
+        self.__log.i(LogType.BROADCAST, "BroadCast On")
         while not self.__broadcastServerThreadEvent.is_set():
             msg = {
                 'ip': '0.0.0.0',
@@ -108,53 +97,53 @@ class Robot:
             sock.sendto(json.dumps(msg).encode('utf8'), ('255.255.255.255', 5891))
             time.sleep(5)
         sock.close()
-        self.system.event.i(LogType.BROADCAST, "Broadcast Thread Close")
+        self.__log.i(LogType.BROADCAST, "Broadcast Thread Close")
 
     def run(self):
-        self.__broadcastServerThread = threading.Thread(target=self.broadcast, daemon=True)
-        self.__broadcastServerThread.start()
-        self.system.event.i(LogType.BROADCAST, "Start Broadcast Thread")
+        #self.__broadcastServerThread = threading.Thread(target=self.broadcast, daemon=True)
+        #self.__broadcastServerThread.start()
+        #self.__log.i(LogType.BROADCAST, "Start Broadcast Thread")
         
         self.__event.run()
 
-        self.__serverThread = mp.Process(target=self.runServer)
-        self.__serverThread.start()
-        self.system.event.i(LogType.ROBOT, "Start Server Thread")
+        #self.__serverThread = mp.Process(target=self.runServer)
+        #self.__serverThread.start()
+        #self.__log.i(LogType.ROBOT, "Start Server Thread")
 
-        self.__aiThread = mp.Process(target=self.runAI, daemon=True)
-        self.__aiThread.start()
-        self.system.event.i(LogType.ROBOT, "Start AI Thread")
+        #self.__aiThread = mp.Process(target=self.runAI, daemon=True)
+        #self.__aiThread.start()
+        #self.__log.i(LogType.ROBOT, "Start AI Thread")
 
         #self.__realTime.connect()
         #self.system.event.i(LogType.REALTIME, "Start RealTime")
 
         self.__backgroundThread = mp.Process(target=self.background, daemon=True)
         self.__backgroundThread.start()
-        self.system.event.i(LogType.ROBOT, "Start Background Thread")
+        self.__log.i(LogType.ROBOT, "Start Background Thread")
 
-        self.__controllerThread = mp.Process(target=self.controller, daemon=True)
-        self.__controllerThread.start()
-        self.system.event.i(LogType.CONTROLLER, "Start Controller Thread")
+        #self.__controllerThread = mp.Process(target=self.controller, daemon=True)
+        #self.__controllerThread.start()
+        #self.__log.i(LogType.CONTROLLER, "Start Controller Thread")
     def close(self):
         #self.__realTime.close()
         self.__event.close()
-        self.system.event.sendEvent('server', {'type':'close'})
-        self.__broadcastServerThreadEvent.set()
-        self.__serverThreadEvent.set()
-        self.__aiThreadEvent.set()
+        #self.__log.sendEvent('server', {'type':'close'})
+        #self.__broadcastServerThreadEvent.set()
+        #self.__serverThreadEvent.set()
+        #self.__aiThreadEvent.set()
         self.__backgroundThreadEvent.set()
-        self.__controllerThreadEvent.set()
+        #self.__controllerThreadEvent.set()
 
-        self.__broadcastServerThread.join()
-        self.__aiThread.join()
-        self.__serverThread.terminate()
-        self.__serverThread.join()
-        self.__controllerThread.join()
-        self.__backgroundThread.join()
+        #self.__broadcastServerThread.join()
+        #self.__serverThread.terminate()
+        #self.__serverThread.join()
+        #self.__aiThread.join()
+        #self.__controllerThread.join()
+        #self.__backgroundThread.join()
 
-        self.__broadcastServerThread.join()
-        self.system.event.i(LogType.ROBOT, "Robot Off")
-        self.system.event.close()
+        #self.__broadcastServerThread.join()
+        #self.__log.i(LogType.ROBOT, "Robot Off")
+        #self.__log.close()
 
 if __name__ == '__main__':
     robot = Robot('ABB Type1', RobotType.ABB, '0.1 Alpha')
