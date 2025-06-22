e78d94a6f81759e08c17e85dd4af31a8cc269c0e
seon0313 <seon06.dev@gmail.com>
2025-06-22 15:04:46 +0900

    update move system

--- controller.py
+++ controller.py
@@ -2,6 +2,8 @@ from system import RobotSystem
 from event import EventListener
 from log import LogType
 from enum import Enum
+from rpiMotorPWM import MotorPWM
+import RPi.GPIO as GPIO
 
 class ValueType(Enum):
     RANGE = 0
@@ -9,27 +11,34 @@ class ValueType(Enum):
 
 
 class Value:
-    def __init__(self, valueType: ValueType, value: any):
+    def __init__(self, valueType: ValueType, minValue: int | float, maxValue: int | float, defaultValue: int | float, controll: MotorPWM):
         self.valueType = valueType
+        self.minValue = minValue
+        self.maxValue = maxValue
+        self.defaultValue = defaultValue
+        self.value = self.defaultValue
+        self.controll = controll
+        GPIO.setmode(GPIO.BOARD)
+    
+    def setValue(self, value):
+        if value < self.minValue: value = self.minValue
+        if value > self.maxValue: value = self.maxValue
         self.value = value
+    
 
 class Controller:
     def __init__(self, system: RobotSystem):
         self.__system: RobotSystem = system
         self.__handle: dict = None
-        self.__moveAngles: dict = {
-            'front': Value(ValueType.RANGE, 0),
-            'back': Value(ValueType.RANGE, 0),
-            'left': Value(ValueType.RANGE, 0),
-            'right': Value(ValueType.RANGE, 0),
-            'rocate': Value(ValueType.VALUE, 0),
-            }
+        self.__motorList: dict[Value] = {}
+        self.__moveAngles: dict[Value] = {}
         self.eventListener: EventListener = EventListener('controller', self.__listener)
     
     def __listener(self, *arg):
         self.__handle = arg[0]
         if self.__handle.get('direction') in self.__moveAngles.keys():
-            self.__moveAngles[self.__handle.get('direction')].value = self.__handle.get('value', 0)
+            self.__moveAngles[self.__handle.get('direction')][0] = self.__handle.get('value', 0)
+            self.__moveAngles[self.__handle.get('direction')][1](self.__moveAngles, self.__motorList)
             self.eventListener.i(LogType.CONTROLLER, f"Move: {self.__handle.get('direction')} value: {self.__moveAngles[self.__handle.get('direction')].value}")
     
     def run(self, event):
@@ -38,10 +47,47 @@ class Controller:
             if self.__handle:
                 self.eventListener.i(LogType.CONTROLLER, f"get Value! {self.__handle}")
                 self.__handle = None
+            
+            for i in self.__motorList.values():
+                i.controll.run(i)
         self.eventListener.close()
 
 class ABBController(Controller):
     def __init__(self, system: RobotSystem):
         super().__init__(system)
+        self.__motorList['top-left'] = Value(ValueType.RANGE, minValue=-255, maxValue=255, defaultValue=0, controll=MotorPWM(8,10,12))
+        #self.__motorList['top-right'] = Value(ValueType.RANGE, minValue=-255, maxValue=255, defaultValue=0, controll=MotorPWM())
+        #self.__motorList['bottom-left'] = Value(ValueType.RANGE, minValue=-255, maxValue=255, defaultValue=0, controll=MotorPWM())
+        #self.__motorList['bottom-right'] = Value(ValueType.RANGE, minValue=-255, maxValue=255, defaultValue=0, controll=MotorPWM())
+
+        self.__moveAngles['front'] = [0, self.moveFront]
+        self.__moveAngles['bottom'] = [0, self.moveBottom]
+        self.__moveAngles['left'] = [0, self.moveLeft]
+        self.__moveAngles['right'] = [0, self.moveBottom]
+
+    def moveFront(self, angles: dict, motors: dict):
+        speed = angles['front'][0]
+        for motor in motors.values():
+            motor: Value = motor
+            motor.setValue(speed)
+
+    def moveBottom(self, angles: dict, motors: dict):
+        speed = angles['bottom'][0]
+        for motor in motors.values():
+            motor: Value = motor
+            motor.setValue(-speed)
+
+    def moveLeft(self, angles: dict, motors: dict):
+        speed = angles['left'][0]
+        for motor_ in motors.keys():
+            motor: Value = motors[motor_]
+            motor.setValue(speed if 'left' in motor_ else -speed)
+
+    def moveRight(self, angles: dict, motors: dict):
+        speed = angles['right'][0]
+        for motor_ in motors.keys():
+            motor: Value = motors[motor_]
+            motor.setValue(-speed if 'left' in motor_ else speed)
+
     def run(self, event):
         return super().run(event)
---
+++ rpiMotorPWM.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+from controller import Value
+
+class MotorPWM:
+    def __init__(self,motorPinEN: int, motorPinA: int, motorPinB: int, reverse: bool = False):
+        self.__pinA: int = motorPinA
+        self.__pinB: int = motorPinB
+        self.__pinEN: int = motorPinEN
+
+        self.reverse: bool = reverse
+
+        GPIO.setup(self.__pinA, GPIO.OUT)
+        GPIO.setup(self.__pinB, GPIO.OUT)
+
+        self.__pwm: GPIO.PWM = GPIO.PWM(self.__pinEN, 100)
+        self.__pwm.start(0)
+    
+    def run(self,value: Value):
+        self.__pwm.ChangeDutyCycle(value.value)
+
+        if self.reverse:
+            GPIO.output(self.__pinA, 1)
+            GPIO.output(self.__pinB, 0)
+        else:
+            GPIO.output(self.__pinA, 0)
+            GPIO.output(self.__pinB, 1)
+        
+        if value.value == value.defaultValue:
+            GPIO.output(self.__pinA, 0)
+            GPIO.output(self.__pinB, 0)
+        
+
