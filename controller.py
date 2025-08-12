fd7a88e3d034cbc63085daffdc18e512790cb3de
seon0313 <seon06.dev@gmail.com>
2025-08-12 23:17:51 +0900

    update

--- controller.py
+++ controller.py
@@ -16,10 +16,10 @@ class Controller:
 
         self.__motorList: dict = {
             'top-left' : Motor(10,12,enable=8),
+            'top-right': Motor(16,18,enable=8),
+            'bottom-left' : Motor(22,24,enable=8),
+            'bottom-right' : Motor(32,36,enable=8),
         }
-        #self.__motorList['top-right'] = Value(ValueType.RANGE, minValue=-255, maxValue=255, defaultValue=0, controll=MotorPWM())
-        #self.__motorList['bottom-left'] = Value(ValueType.RANGE, minValue=-255, maxValue=255, defaultValue=0, controll=MotorPWM())
-        #self.__motorList['bottom-right'] = Value(ValueType.RANGE, minValue=-255, maxValue=255, defaultValue=0, controll=MotorPWM())
 
         self.__moveAngles: dict = {
             'front': [0, self.moveFront],
@@ -32,17 +32,18 @@ class Controller:
         self.__handle = arg[0]
         self.eventListener.i(LogType.CONTROLLER, f"Controller: {self.__handle}")
         self.eventListener.i(LogType.CONTROLLER, f"Controller: {type(self.__handle)}")
-        if self.__handle.get('direction') in self.__moveAngles.keys():
-            self.__moveAngles[self.__handle.get('direction')][0] = self.__handle.get('value', 0)
-            self.__moveAngles[self.__handle.get('direction')][1](self.__moveAngles, self.__motorList)
-            self.eventListener.i(LogType.CONTROLLER, f"Move: {self.__handle.get('direction')} value: {self.__moveAngles[self.__handle.get('direction')]}")
+
         
     def run(self, event):
         self.eventListener.run()
         while not event.is_set():
             if self.__handle:
                 self.eventListener.i(LogType.CONTROLLER, f"get Value! {self.__handle}")
-                #self.__handle = None
+                if self.__handle.get('direction') in self.__moveAngles.keys():
+                    self.__moveAngles[self.__handle.get('direction')][0] = self.__handle.get('value', 0)
+                    self.__moveAngles[self.__handle.get('direction')][1](self.__moveAngles, self.__motorList)
+                    print(f"Move: {self.__handle.get('direction')} value: {self.__motorList['top-left'].value}", flush=True)
+                self.__handle = None
         self.eventListener.close()
     
     def close(self):
